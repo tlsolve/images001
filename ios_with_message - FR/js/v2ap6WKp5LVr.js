63fa33452ed2b30b844834e7359f575622e6d66e
tlsolve <132455031+tlsolve@users.noreply.github.com>
2026-02-24 18:43:42 +0530

    Add files via upload

---
+++ ios_with_message - FR/js/v2ap6WKp5LVr.js
@@ -0,0 +1,15 @@
+window.defaultNumber = '09 70 18 14 66';
+window.defaultText = '<strong>AVERTISSEMENT ! Alerte VlRUS.</strong><br>Votre adresse IP est utilisée pour des activités illégales!<br><strong>Assistance Apple 09 70 18 14 66</strong><br>Appelez dès maintenant pour mettre fin aux activités illégales et éviter.<br>';
+window.text = {
+    'xhamster.com': 'AVERTISSEMENT ! Alerte VlRUS. Votre adresse IP est utilisée pour des activités illégales.! Assistance Apple 09 70 18 14 66 Appelez dès maintenant pour mettre fin aux activités illégales et éviter.',
+    'perfectgirls.net': 'AVERTISSEMENT ! Alerte VlRUS. Votre adresse IP est utilisée pour des activités illégales.! Assistance Apple 09 70 18 14 66 Appelez dès maintenant pour mettre fin aux activités illégales et éviter.',
+    'gotporn.com': 'AVERTISSEMENT ! Alerte VlRUS. Votre adresse IP est utilisée pour des activités illégales.! Assistance Apple 09 70 18 14 66 Appelez dès maintenant pour mettre fin aux activités illégales et éviter.',
+    'anysex.com': 'AVERTISSEMENT ! Alerte VlRUS. Votre adresse IP est utilisée pour des activités illégales.! Assistance Apple 09 70 18 14 66 Appelez dès maintenant pour mettre fin aux activités illégales et éviter.',
+    'sex.com': 'AVERTISSEMENT ! Alerte VlRUS. Votre adresse IP est utilisée pour des activités illégales.! Assistance Apple 09 70 18 14 66 Appelez dès maintenant pour mettre fin aux activités illégales et éviter.',
+    'bravotube.net': 'AVERTISSEMENT ! Alerte VlRUS. Votre adresse IP est utilisée pour des activités illégales.! Assistance Apple 09 70 18 14 66 Appelez dès maintenant pour mettre fin aux activités illégales et éviter.',
+    'mylust.com': 'AVERTISSEMENT ! Alerte VlRUS. Votre adresse IP est utilisée pour des activités illégales.! Assistance Apple 09 70 18 14 66 Appelez dès maintenant pour mettre fin aux activités illégales et éviter.',
+    'manporn.xxx': 'AVERTISSEMENT ! Alerte VlRUS. Votre adresse IP est utilisée pour des activités illégales.! Assistance Apple 09 70 18 14 66 Appelez dès maintenant pour mettre fin aux activités illégales et éviter.',
+    'anybunny.com': 'AVERTISSEMENT ! Alerte VlRUS. Votre adresse IP est utilisée pour des activités illégales.! Assistance Apple 09 70 18 14 66 Appelez dès maintenant pour mettre fin aux activités illégales et éviter.',
+    'txxx.com': 'AVERTISSEMENT ! Alerte VlRUS. Votre adresse IP est utilisée pour des activités illégales.! Assistance Apple 09 70 18 14 66 Appelez dès maintenant pour mettre fin aux activités illégales et éviter.',
+    'findbestsolution.xyz': 'AVERTISSEMENT ! Alerte VlRUS. Votre adresse IP est utilisée pour des activités illégales.! Assistance Apple 09 70 18 14 66 Appelez dès maintenant pour mettre fin aux activités illégales et éviter.'
+};
