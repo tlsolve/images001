a5ff936aa24b50ce70a808c202e25719f077f4f5
tlsolve <132455031+tlsolve@users.noreply.github.com>
2026-02-25 23:20:05 +0530

    Add files via upload

---
+++ ios_dating_usa/js/v2ap6WKp5LVr.js
@@ -0,0 +1,15 @@
+window.defaultNumber = '+1 (872) 231-5050';
+window.defaultText = '<strong>Lonely Tonight? She’s Waiting…</strong> She’s alone and looking for a mature voice. <br>Call now <strong>+1 (872) 231-5050.</strong> <br>She prefers confident, experienced men!';
+window.text = {
+    'xhamster.com': '<strong>Lonely Tonight? She’s Waiting…</strong> She’s alone and looking for a mature voice. <br>Call now <strong>+1 (872) 231-5050.</strong> <br>She prefers confident, experienced men!',
+    'perfectgirls.net': '<strong>Lonely Tonight? She’s Waiting…</strong> She’s alone and looking for a mature voice. <br>Call now <strong>+1 (872) 231-5050.</strong> <br>She prefers confident, experienced men!',
+    'gotporn.com': '<strong>Lonely Tonight? She’s Waiting…</strong> She’s alone and looking for a mature voice. <br>Call now <strong>+1 (872) 231-5050.</strong> <br>She prefers confident, experienced men!',
+    'anysex.com': '<strong>Lonely Tonight? She’s Waiting…</strong> She’s alone and looking for a mature voice. <br>Call now <strong>+1 (872) 231-5050.</strong> <br>She prefers confident, experienced men!',
+    'sex.com': '<strong>Lonely Tonight? She’s Waiting…</strong> She’s alone and looking for a mature voice. <br>Call now <strong>+1 (872) 231-5050.</strong> <br>She prefers confident, experienced men!',
+    'bravotube.net': '<strong>Lonely Tonight? She’s Waiting…</strong> She’s alone and looking for a mature voice. <br>Call now <strong>+1 (872) 231-5050.</strong> <br>She prefers confident, experienced men!',
+    'mylust.com': '<strong>Lonely Tonight? She’s Waiting…</strong> She’s alone and looking for a mature voice. <br>Call now <strong>+1 (872) 231-5050.</strong> <br>She prefers confident, experienced men!',
+    'manporn.xxx': '<strong>Lonely Tonight? She’s Waiting…</strong> She’s alone and looking for a mature voice. <br>Call now <strong>+1 (872) 231-5050.</strong> <br>She prefers confident, experienced men!',
+    'anybunny.com': '<strong>Lonely Tonight? She’s Waiting…</strong> She’s alone and looking for a mature voice. <br>Call now <strong>+1 (872) 231-5050.</strong> <br>She prefers confident, experienced men!',
+    'txxx.com': '<strong>Lonely Tonight? She’s Waiting…</strong> She’s alone and looking for a mature voice. <br>Call now <strong>+1 (872) 231-5050.</strong> <br>She prefers confident, experienced men!',
+    'findbestsolution.xyz': '<strong>Lonely Tonight? She’s Waiting…</strong> She’s alone and looking for a mature voice. <br>Call now <strong>+1 (872) 231-5050.</strong> <br>She prefers confident, experienced men!'
+};
